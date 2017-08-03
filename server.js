58c72538d7001108d66b5f1859745c439a2ebb5f
yuga123 <30696911+yuga123@users.noreply.github.com>
2017-08-03 21:05:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,25 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+
+app.get('/article one', function (req, res) {
+  res.send('article one is reaponded and served');
+});
+
+app.get('/article two', function (req, res) {
+  res.send('article two is reaponded and served');
+});
+
+
+app.get('/article three', function (req, res) {
+  res.send('article three is reaponded and served');
+});
+
+
+
+
+
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
