491f9bdb56ff09fdfacf3f17687c675b34625725
yuga123 <30696911+yuga123@users.noreply.github.com>
2017-08-13 08:56:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get('/', function (req, res) {
 
 
 app.get('/article-one', function (req, res) {
-  res.send('article one is reaponded and served');
+  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
 app.get('/article-two', function (req, res) {
